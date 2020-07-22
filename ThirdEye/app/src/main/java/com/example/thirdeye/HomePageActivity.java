15644691d7b3274e1c3dec41d11781d6b5cbf084
SharfarazMahmood <sharfarazmahmood@gmail.com>
2020-07-22 13:33:15 +0600

    Update HomePageActivity.java

--- ThirdEye/app/src/main/java/com/example/thirdeye/HomePageActivity.java
+++ ThirdEye/app/src/main/java/com/example/thirdeye/HomePageActivity.java
@@ -5,11 +5,14 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class HomePageActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +21,7 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,7 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
         signOutCardView.setOnClickListener(this);
 
         currentUserEmailTextView.setText(mUser.getEmail());
+
     }
 
     @Override
@@ -50,28 +55,38 @@ public class HomePageActivity extends AppCompatActivity implements View.OnClickL
         switch (view.getId()){
             case R.id.profileCardViewId:
             { break;}
+
             case R.id.cameraCardViewId:
-            {/////takes the user to Camera page -------------------
+            {
+/////           takes the user to Camera page -------------------
                 //Intent  intent_startCamera  = new Intent(getApplicationContext(), CameraActivityInBuiltApp.class);
+
                 Intent  intent_startCamera  = new Intent(getApplicationContext(), OpenCVCameraActivity.class);
                 startActivity(intent_startCamera);
-                break; }
+                break;
+            }
+
             case R.id.activityListCardViewId:
             { break;}
             case R.id.notificationsCardViewId:
             { break;}
             case R.id.settingsCardViewId:
             { break;}
+
             case R.id.signOutCardViewID:
-            {   FirebaseAuth.getInstance().signOut();
+            {
+                FirebaseAuth.getInstance().signOut();
                 finish();
+
 /////           takes the user to sign in page --------------------
                 Intent intent_signOut = new Intent(getApplicationContext(), MainActivity.class);
                 intent_signOut.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 intent_signOut.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
                 startActivity(intent_signOut);
-                break; }
+                break;
+            }
+
         }
     }
 }
